905293ad6955ea686fd71e954608c18fb63161cc
marisofip <115109076+marisofip@users.noreply.github.com>
2022-12-08 12:57:27 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-let form = document.getElementById("form");
-
-form.addEventListener("submit", evento => {
-  evento.preventDefault();
-});
